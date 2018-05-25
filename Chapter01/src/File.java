671d0dab741ed9cac7232e027d1ca48f638eb3d6
USER <USER@PRO-05>
2018-05-25 11:50:25 +0900

    3212

---
+++ Chapter01/src/File.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class File {
+
+
+		static class Address{
+		    String name;
+		    String ip;
+		    String number;
+		    int age;
+
+	    
+	    Address(String name, String ip, String number, int age) {
+	    	
+	    	this.name = name;
+	    	this.ip = ip; 
+	    	this.number = number;
+	        this.age = age;
+	    	
+	    	}
+		}
+		static void print() { //출력
+			
+			
+		}
+		static void view() {  //전체
+			
+		}
+		static void add() {  //추가
+			
+		}
+		static void delete() { //삭제
+			
+		}
+		
+		public static void main(String[] args) {
+			
+			
+			
+		}
+	}
